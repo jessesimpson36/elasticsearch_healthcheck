c0eeb723490eb6b67b09012f65890c96b88f5e48
Jesse Simpson <jesse.simpson@camunda.com>
2024-01-04 11:42:29 -0500

    init

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import com.sun.net.httpserver.HttpServer;
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.admin.cluster.health.ClusterHealthRequest;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.RequestOptions;
+
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        HttpServer server = null;
+        try {
+            server = HttpServer.create(new InetSocketAddress("localhost", 8001), 0);
+        } catch (java.io.IOException e) {
+            System.out.println("Error occurred");
+            System.exit(1);
+        }
+        ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) Executors.newFixedThreadPool(10);
+
+        server.createContext("/healthcheck", new  MyHttpHandler());
+        server.setExecutor(threadPoolExecutor);
+
+        System.out.println(" Server started on port 8001");
+        server.start();
+    }
+}
---
+++ src/main/java/org/example/MyHttpHandler.java
@@ -0,0 +1,74 @@
+package org.example;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.admin.cluster.health.ClusterHealthRequest;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class MyHttpHandler implements HttpHandler {
+
+    @Override
+    public void handle(HttpExchange httpExchange) throws IOException {
+        String response=null;
+        if("GET".equals(httpExchange.getRequestMethod())) {
+            response = handleGetRequest(httpExchange);
+        }
+        handleResponse(httpExchange,response);
+    }
+    private String handleGetRequest(HttpExchange httpExchange) {
+        final RestHighLevelClient esClient = new RestHighLevelClient(
+                RestClient.builder(
+                        new HttpHost("localhost", 9200, "http")
+                )
+        );
+        int numberOfClusterNodes = 0;
+
+        try {
+            numberOfClusterNodes = esClient.cluster()
+                    .health(new ClusterHealthRequest(), RequestOptions.DEFAULT)
+                    .getNumberOfNodes();
+        } catch (java.io.IOException e) {
+            System.out.println("Error occurred");
+            return "";
+        }
+
+        System.out.println("Number of cluster nodes: " + String.valueOf(numberOfClusterNodes));
+
+        try {
+            esClient.close();
+        } catch (java.io.IOException e) {
+            System.out.println("Error occurred");
+            return "";
+        }
+
+        return String.valueOf(numberOfClusterNodes);
+    }
+    private void handleResponse(HttpExchange httpExchange, String response)  throws  IOException {
+        int statusCode = 200;
+        String message = response;
+        if (response.isEmpty()) {
+            statusCode = 400;
+            message = "Error";
+        }
+        OutputStream outputStream = httpExchange.getResponseBody();
+        StringBuilder htmlBuilder = new StringBuilder();
+        htmlBuilder.append("{").
+        append("\"cluster_number\": ")
+                .append(message)
+                .append("}");
+        // encode HTML content
+        String htmlResponse = htmlBuilder.toString();
+        // this line is a must
+        httpExchange.sendResponseHeaders(statusCode, htmlResponse.length());
+        outputStream.write(htmlResponse.getBytes());
+        outputStream.flush();
+        outputStream.close();
+    }
+}
+
